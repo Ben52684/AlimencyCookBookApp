17cdc3aa5e8098ab6b08280abef024b57d0dc7e1
Benjamin <balberts023@student.wethinkcode.co.za>
2024-11-11 13:07:16 +0200

    Added basic Home screen view

---
+++ alimency_cook_book/lib/src/home.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  static const routeName = '/home';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 197, 223, 50),
+      appBar: AppBar(title: const Text("Alimency"), backgroundColor: const Color.fromARGB(255, 143, 131, 131), centerTitle: true,),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    child: Card(
+                        child: SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: FloatingActionButton(
+                      child: Icon(Icons.favorite), onPressed: () {}),
+                ))),
+                Container(
+                    child: Card(
+                        child: SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: FloatingActionButton(
+                      child: Icon(Icons.library_books), onPressed: () {}),
+                )))
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    child: Card(
+                        child: SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: FloatingActionButton(
+                      child: Icon(Icons.history), onPressed: () {}),
+                ))),
+                Container(
+                    child: Card(
+                        child: SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: FloatingActionButton(
+                      child: Icon(Icons.food_bank), onPressed: () {}),
+                )))
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: SizedBox(
+                    height: 100,
+                    width: 100,
+                    child: FloatingActionButton(
+                      child: Icon(Icons.circle),
+                      onPressed: () {},
+                      shape: CircleBorder(),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
